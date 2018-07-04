51fff4687c782dabc11d164f243356b638c46e2c
Cicely Beckford <cicelybeckford@fb.com>
2018-07-04 07:03:12 -0700

    Modified Compose Activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,11 +3,17 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.models.User;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONException;
@@ -21,6 +27,10 @@ public class ComposeActivity extends AppCompatActivity {
     private String tweetMessage;
     Button btnTweet;
     EditText twInput;
+    TextView tvTHandle;
+    TextView tvTName;
+    ImageView ivProfile;
+    TextView tvCharCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +38,62 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose);
         btnTweet = (Button) findViewById(R.id.btnTweet);
         twInput = (EditText) findViewById(R.id.twInput);
+        tvTHandle = (TextView) findViewById(R.id.tvTHandle);
+        tvTName = (TextView) findViewById(R.id.tvTName);
+        ivProfile = (ImageView) findViewById(R.id.ivProfile);
+        tvCharCount = (TextView) findViewById(R.id.tvCharCount);
 
         client = TwitterApp.getRestClient(this);
+        userInfo();
+
+        tvCharCount.setText("140");
+        inputCount(twInput, tvCharCount);
+
         postTweet(btnTweet);
+    }
+
+    public void inputCount (EditText editText, final TextView textView) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String input = s.toString();
+                int remaining = 140 - input.length();
+                textView.setText("" + remaining);
+            }
+        });
+    }
+
+    public void cancelTweet (View view) { finish();}
+
+    public void userInfo () {
+        client.getCurrentUser(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    User user = User.fromJSON(response);
+                    tvTHandle.setText("@" + user.screenName);
+                    tvTName.setText(user.name);
+                    Glide.with(getApplicationContext()).load(user.profileImageurl).into(ivProfile);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
     }
 
     public void postTweet(View view) {
--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -59,6 +59,12 @@ public class TwitterClient extends OAuthBaseClient {
 		client.post(apiUrl, params, handler);
 	}
 
+	public void getCurrentUser(AsyncHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("account/verify_credentials.json");
+		RequestParams params = new RequestParams();
+		client.get(apiUrl, params, handler);
+	}
+
 	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
 	 * 	  i.e getApiUrl("statuses/home_timeline.json");
 	 * 2. Define the parameters to pass to the request (query or body)
